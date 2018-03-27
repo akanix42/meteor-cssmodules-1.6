1cef471581bdd93ebfc3c7d2b27cf2347c6bdb1f
csKangGdcomm <37207938+csKangGdcomm@users.noreply.github.com>
2018-03-27 11:55:36 +0900

    minor bug fix

--- imports/app.jsx
+++ imports/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './app.css'
 
-const App = () => <div className={styles.app}> hey</div>
+const App = () => <div className={styles.styles.app}> hey</div>
 
 export default App
